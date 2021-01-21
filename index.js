866fd1c41bd81cb6c19199e06a835f815a81570e
Chris Burton <chris@fullstackcaveman.com>
2021-01-21 13:23:15 -0700

    Solved Task:5

--- index.js
+++ index.js
@@ -128,6 +128,11 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
+reviews[7]['feedback'] =
+	'this place is chill with really cool people, great for getting work done on weekdays';
+
+console.log(reviews);
+
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
 
